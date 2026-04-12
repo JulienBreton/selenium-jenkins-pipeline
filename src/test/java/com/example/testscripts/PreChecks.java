ae873552ca62662e20da8e5881e7950a534b2078
JulienBreton <julienbreton@hotmail.fr>
2026-04-12 15:12:59 +0200

    pre-checks : on vérifie que la ptf est ok

--- src/test/java/com/example/testscripts/PreChecks.java
+++ src/test/java/com/example/testscripts/PreChecks.java
@@ -8,7 +8,6 @@ public class PreChecks  extends TestBase {
     
     @Test
     public void verifyStatutPtfOK() {
-        // Le driver est déjà initialisé par le @BeforeMethod de TestBase
         driver.get("https://julienbreton.github.io/selenium-jenkins-pipeline/statut_ptf?status=ok");
         String statut = driver.findElement(By.id("status-message")).getText();
         System.out.print("✅ Vérification du statut de la plateforme : " + statut);
@@ -17,10 +16,10 @@ public class PreChecks  extends TestBase {
 
     @Test
     public void verifyStatutPtfKO() {
-        // Le driver est déjà initialisé par le @BeforeMethod de TestBase
         driver.get("https://julienbreton.github.io/selenium-jenkins-pipeline/statut_ptf?status=ko");
         String statut = driver.findElement(By.id("status-message")).getText();
-        System.out.print("❌ Vérification du statut de la plateforme : " + statut);        
-        Assert.assertTrue(statut.contains("la plateforme est KO"));
+        System.out.print("❌ Vérification du statut de la plateforme : " + statut);    
+        //On s'attend à ce que la ptf soit ok avant de démarrer les tests.    
+        Assert.assertTrue(statut.contains("la plateforme est ok"));
     }
 }
