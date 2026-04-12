f415e2b86e3f7ca31b6d62aba9262874c07d14ea
JulienBreton <julienbreton@hotmail.fr>
2026-04-12 14:47:00 +0200

    conf : vérifier état ptf

---
+++ src/test/java/com/example/testscripts/PreChecks.java
@@ -0,0 +1,24 @@
+package com.example.testscripts;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PreChecks  extends TestBase {
+    
+    @Test
+    public void verifyStatutPtfOK() {
+        // Le driver est déjà initialisé par le @BeforeMethod de TestBase
+        driver.get("https://julienbreton.github.io/selenium-jenkins-pipeline/ptf_ok.html?status=ok");
+        String statut = driver.findElement(By.id("status-message")).getText();
+        Assert.assertTrue(statut.contains("la plateforme est ok"));
+    }
+
+    @Test
+    public void verifyStatutPtfKO() {
+        // Le driver est déjà initialisé par le @BeforeMethod de TestBase
+        driver.get("https://julienbreton.github.io/selenium-jenkins-pipeline/ptf_ok.html?status=ko");
+        String statut = driver.findElement(By.id("status-message")).getText();
+        Assert.assertTrue(statut.contains("la plateforme est KO"));
+    }
+}
