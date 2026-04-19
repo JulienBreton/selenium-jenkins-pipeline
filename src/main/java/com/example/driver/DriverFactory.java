444b460a10e579cce912010ed725746679731ad1
JulienBreton <julienbreton@hotmail.fr>
2026-04-19 16:25:57 +0200

    conf : se connecte à pc-host depuis la vm

--- src/main/java/com/example/driver/DriverFactory.java
+++ src/main/java/com/example/driver/DriverFactory.java
@@ -19,7 +19,7 @@ public class DriverFactory {
         if ("remote".equalsIgnoreCase(runMode)) {
             System.out.println("🌐 Connexion au Selenium Hub Docker...");
             return new RemoteWebDriver(
-                    URI.create("pc-hote:4444/wd/hub").toURL(),
+                    URI.create("http://pc-hote:4444/wd/hub").toURL(),
                     options);
         } else {
             System.out.println("💻 Lancement du Chrome local...");
